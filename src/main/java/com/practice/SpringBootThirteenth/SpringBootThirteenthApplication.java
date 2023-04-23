bd741979972f27917ddcebe696bff42727dae2a9
abhishekgadhia1 <abhishek.gadhia@gmail.com>
2023-04-23 14:57:53 +0530

    first

---
+++ src/main/java/com/practice/SpringBootThirteenth/SpringBootThirteenthApplication.java
@@ -0,0 +1,14 @@
+package com.practice.SpringBootThirteenth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringBootThirteenthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThirteenthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/SpringBootThirteenth/SpringBootThirteenthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.SpringBootThirteenth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThirteenthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
